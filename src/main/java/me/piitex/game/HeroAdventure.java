aac4e9b04ec5b26b36b2eaedc8d119f2fee9c55b
HackusatePvP <supermftw@gmail.com>
2024-06-14 14:46:19 -0500

    Updated to support latest RenJava build.

--- src/main/java/me/piitex/game/HeroAdventure.java
+++ src/main/java/me/piitex/game/HeroAdventure.java
@@ -13,6 +13,7 @@ import me.piitex.renjava.api.loaders.FontLoader;
 import me.piitex.renjava.configuration.Configuration;
 import me.piitex.renjava.configuration.RenJavaConfiguration;
 import me.piitex.renjava.gui.Menu;
+import me.piitex.renjava.gui.StageType;
 import me.piitex.renjava.gui.layouts.impl.VerticalLayout;
 import me.piitex.renjava.gui.overlay.ButtonOverlay;
 import me.piitex.renjava.gui.overlay.ImageOverlay;
@@ -62,7 +63,7 @@ public class HeroAdventure extends RenJava {
     }
 
     @Override
-    public Menu buildTitleScreen() {
+    public Menu buildTitleScreen(boolean rightClick) {
         Menu menu = new Menu(1920, 1080, new ImageOverlay("gui/main_menu.png"));
 
         TextOverlay gameText = new TextOverlay(name + ' ' + version, new FontLoader(getConfiguration().getUiFont(), 36), 1500, 975);
@@ -73,29 +74,45 @@ public class HeroAdventure extends RenJava {
     }
 
     @Override
-    public Menu buildSideMenu() {
+    public Menu buildSideMenu(boolean rightClickedMenu) {
         Menu menu = new Menu(1920, 1080, new ImageOverlay("gui/overlay/main_menu.png"));
 
         Font uiFont = RenJava.getInstance().getConfiguration().getUiFont().getFont();
 
         Color hoverColor = getConfiguration().getHoverColor();
 
-        ButtonOverlay startButton = new ButtonOverlay("menu-start-button", "Start", uiFont, Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
-        ButtonOverlay loadButton = new ButtonOverlay("menu-load-button", "Load", uiFont, Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
-        ButtonOverlay saveButton = new ButtonOverlay("menu-save-button", "Save", uiFont, Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
-        ButtonOverlay optionsButton = new ButtonOverlay("menu-preference-button", "Preferences", uiFont, Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
-        ButtonOverlay aboutButton = new ButtonOverlay("menu-about-button", "About", uiFont, Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
-        ButtonOverlay donateButton = new ButtonOverlay("menu-donate-button", "Donate", uiFont, Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        ButtonOverlay startButton = new ButtonOverlay("menu-start-button", "Start", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        ButtonOverlay loadButton = new ButtonOverlay("menu-load-button", "Load", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        ButtonOverlay saveButton = new ButtonOverlay("menu-save-button", "Save", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        ButtonOverlay optionsButton = new ButtonOverlay("menu-preference-button", "Preferences", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        ButtonOverlay aboutButton = new ButtonOverlay("menu-about-button", "About", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        ButtonOverlay donateButton = new ButtonOverlay("menu-donate-button", "Donate", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
 
         // Create vbox for the buttons. You can also do an HBox
         VerticalLayout layout = new VerticalLayout(400, 500);
         layout.setX(50);
         layout.setY(250);
         layout.setSpacing(20);
-        layout.addOverlays(startButton, loadButton, saveButton, donateButton, optionsButton, aboutButton);
+        layout.addOverlays(startButton, loadButton);
+        if (rightClickedMenu) {
+            layout.addOverlays(saveButton);
+        }
+        layout.addOverlays(optionsButton, aboutButton, donateButton);
 
         // You don't have to add the button overlays just add the layout which already contains the overlays.
         menu.addLayout(layout);
+
+        ButtonOverlay returnButton;
+
+
+        if (getStageType() == StageType.MAIN_MENU) {
+            returnButton = new ButtonOverlay("menu-quit-button", "Quit", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        } else {
+            returnButton = new ButtonOverlay("menu-return-button", "Return", Color.BLACK, uiFont, Color.TRANSPARENT, Color.TRANSPARENT, hoverColor, 1, 1);
+        }
+        returnButton.setX(25);
+        returnButton.setY(1000);
+        menu.addOverlay(returnButton);
 
         return menu;
     }
