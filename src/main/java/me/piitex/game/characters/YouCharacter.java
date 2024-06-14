eca5388157b644b77aa84881bac4a212cce0eb50
HackusatePvP <supermftw@gmail.com>
2024-06-14 15:03:35 -0500

    Added data to the main character.

--- src/main/java/me/piitex/game/characters/YouCharacter.java
+++ src/main/java/me/piitex/game/characters/YouCharacter.java
@@ -2,10 +2,21 @@ package me.piitex.game.characters;
 
 import javafx.scene.paint.Color;
 import me.piitex.renjava.api.characters.Character;
+import me.piitex.renjava.api.saves.data.Data;
+import me.piitex.renjava.api.saves.data.PersistentData;
 
-public class YouCharacter extends Character {
+public class YouCharacter extends Character implements PersistentData {
+    @Data private int gold;
 
     public YouCharacter() {
         super("mc", "You", Color.WHITE);
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
     }
 }
