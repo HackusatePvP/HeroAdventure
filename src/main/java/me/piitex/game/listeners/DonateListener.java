f6444db26de39046413461076899e87afae7b25f
HackusatePvP <no@email.com>
2024-11-13 05:23:36 -0600

    Changed to static call.

--- src/main/java/me/piitex/game/listeners/DonateListener.java
+++ src/main/java/me/piitex/game/listeners/DonateListener.java
@@ -1,12 +1,9 @@
 package me.piitex.game.listeners;
 
 import me.piitex.game.HeroAdventure;
-import me.piitex.renjava.RenJava;
 import me.piitex.renjava.events.EventListener;
 import me.piitex.renjava.events.Listener;
 import me.piitex.renjava.events.types.ButtonClickEvent;
-import me.piitex.renjava.gui.Container;
-import me.piitex.renjava.gui.Window;
 
 public class DonateListener implements EventListener {
 
@@ -15,7 +12,7 @@ public class DonateListener implements EventListener {
         // Handle button click event
         if (event.getButton().getId().equalsIgnoreCase("menu-donate-button")) {
             // Open link
-            HeroAdventure.getInstance().getHost().showDocument("https://google.com/");
+            HeroAdventure.openLink("https://google.com/");
         }
     }
 }
